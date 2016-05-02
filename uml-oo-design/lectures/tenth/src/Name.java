8119e4a239a568e2b50adbef5b9c051d187cd7ba
dprasad <dprasadm@gmail.com>
2016-05-02 13:24:28 +0530

    Moved content files and source code to separate directories.

---
+++ uml-oo-design/lectures/tenth/src/Name.java
@@ -0,0 +1,10 @@
+class Name {
+	private String name;
+
+    // ctor is visible only in this package!
+	Name(String s) {
+		name = s;
+	}
+
+	String getName() { return name; }
+}
---
+++ uml-oo-design/lectures/tenth/src/run.sh
@@ -0,0 +1,1 @@
+java -ea -cp .:json.jar NameMaker
