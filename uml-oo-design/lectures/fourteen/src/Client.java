09a24d89fce890f6f1ab1cbdc957cc9a6aebe06e
dprasad <dprasadm@gmail.com>
2016-05-19 12:27:14 +0530

    Added one more test case just so it is easy to explain.

--- uml-oo-design/lectures/fourteen/src/Client.java
+++ uml-oo-design/lectures/fourteen/src/Client.java
@@ -1,4 +1,17 @@
 class Client {
+    private static void testOneAddRenameRemove() {
+        Directory dir = new Directory();
+        LoggingObserver lobs = new LoggingObserver();
+        lobs.startObserving(dir);
+
+        Item one = new Item("1");
+        dir.add(one); 
+        one = dir.rename(one, "3");
+        dir.remove(one);
+
+        lobs.stopObserving(dir);
+    }
+
 	private static void testLoggingObserver() {
         Directory dir = new Directory();
         LoggingObserver lobs = new LoggingObserver();
@@ -8,17 +21,17 @@ class Client {
         Item two = new Item("2");
         dir.add(one);
         dir.add(two);
-        
-        lobs.stopObserving(dir);
 
         one = dir.rename(one, "3");
         one = dir.rename(one, "5");
-
         dir.remove(two);
         dir.remove(one);
+
+        lobs.stopObserving(dir);
 	}
 
     public static void main(String args[]) {
-        testLoggingObserver();
+        testOneAddRenameRemove();
+        //testLoggingObserver();
     }
 }
